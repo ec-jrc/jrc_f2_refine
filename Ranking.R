d38e36cf478aead2e93a3bd9ad9ff2073708afbb
ROLLAND Etienne (JRC-ISPRA) <rollaet@S-JRCSVQRS003P.net1.cec.eu.int>
2019-12-11 11:52:25 +0100

    func to initiate the dataframe

--- Ranking.R
+++ Ranking.R
@@ -5,11 +5,43 @@ library(stringr)
 library(tabulizer)
 library(tm)
 
+create_quality_df <- function(material_characterisation) {
+  #this function create the dataframe quality_evaluation that store the informations
+  #of the quality evalution assessment
+  
+  #nb_technical_aspect<-length(material_characterisation)
+  technical_aspect<-names(material_characterisation)
+  names_columns<-c("Article_name", technical_aspect)
+  
+  
+  quality_evaluation_df<-as.data.frame(matrix(ncol=length(names_columns), 
+                                              byrow=TRUE))
+  
+  colnames(quality_evaluation_df)<-names_columns
+  
+  return(quality_evaluation_df)
+}
 
-#nm and nm^2 ?
+init_quality_df <- function(quality_evaluation_df, pdf_name) {
+  #create empty dataframe is such a pain in R
+  #initiate the quality_df dataframe, i.e., add zero everywhere and the pdf name,
+  #for the first iteration of the loop
+  quality_evaluation_df[["Article_name"]]<-pdf_name
+  quality_evaluation_df[is.na(quality_evaluation_df)] = 0
+  
+  return(quality_evaluation_df)
+}
 
-#do pdf name
+is_space <- function(word) {
+  res<-str_split(word, "\\s")[[1]]
+  if (length(res)==2) {
+    return(TRUE)
+  }
+  return(FALSE)
+}
 
+
+#ontology of terms
 material_characterisation <- list( Size = c("diameter", "size", "dimension", "radius"), 
                                    Surface_area = c("surface area"), 
                                    Surface_charge = c("zeta potentiual", "surface charge", "mv"))
@@ -23,9 +55,17 @@ print(tail(unique(x$sentence), 10))
 
 #the func must know pdf_name
 #for loop with it of row
-i<-1
+
+quality_evaluation_df<-create_quality_df(material_characterisation)
+
+init_quality_df(quality_evaluation_df, pdf_name)
+
+
 pdf_name<-"Abrams, M T et al 2010"
-quality_evaluation_df[i]$Article_name<-pdf_name
+
+if (i==1) {
+  quality_evaluation_df<-init_quality_df(quality_evaluation_df, pdf_name)
+}
 
 quality_evaluation_df #add a row for pdf name
 for (technical_aspect in names(material_characterisation) ) { #Size, surface area, etc
@@ -45,35 +85,6 @@ for (technical_aspect in names(material_characterisation) ) { #Size, surface are
   }
 }
 
-
-
-quality_evaluation_df<-create_quality_df(material_characterisation)
-
-create_quality_df <- function(material_characterisation) {
-  #this function create the dataframe quality_evaluation that store the informations
-  #of the quality evalution assessment
-  
-  #nb_technical_aspect<-length(material_characterisation)
-  technical_aspect<-names(material_characterisation)
-  names_columns<-c("Article_name", technical_aspect)
-  
-  
-  quality_evaluation_df<-as.data.frame(matrix(ncol=length(names_columns), 
-                                              byrow=TRUE))
-  
-  colnames(quality_evaluation_df)<-names_columns
-  
-  return(quality_evaluation_df)
-}
-
-
-is_space <- function(word) {
-  res<-str_split(word, "\\s")[[1]]
-  if (length(res)==2) {
-    return(TRUE)
-  }
-  return(FALSE)
-}
 
 
 
