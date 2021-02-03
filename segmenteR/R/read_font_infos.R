6d5cac5aff085b4561aa787be0339adc192eac19
erolland <etienne.rolland.cdk@gmail.com>
2021-02-03 19:14:52 +0100

    Change in code to reflect the new main of pdftools. Test done. Failed because of slight modification of the annotator of udpipe.

--- segmenteR/R/read_font_infos.R
+++ segmenteR/R/read_font_infos.R
@@ -8,7 +8,7 @@ prepare_poppler_output <- function(pdf_name) {
   res <- pdftools::poppler_config()
   stopifnot(res$has_local_font_info)
 
-  poppler_output <- pdftools::pdf_data(pdf_name)
+  poppler_output <- pdftools::pdf_data(pdf_name, font_info = TRUE)
   poppler_output <- do.call(rbind, poppler_output)
   poppler_output <- poppler_output[c("text", "font_name", "font_size")]
   poppler_output <- as.data.frame(poppler_output)
